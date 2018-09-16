087d2621b07598dc50a7657eb3518c5a6f57323f
Manoj Gautam <nix1947@users.noreply.github.com>
2018-09-16 15:00:37 +0545

    Update read_xlsx.py

--- python/spreadsheets/read_xlsx.py
+++ python/spreadsheets/read_xlsx.py
@@ -11,3 +11,40 @@ sheet = wb.active
 # sheet = wb['User Information']
 
 print(sheet['A1'].value)  # Read a specific cell
+
+
+# Reading and parsing spreadsheet by using xlrd module
+import xlrd
+book = xlrd.open_workbook('rate_table.xlsx')
+
+# Get the sheet name by index
+sheet = book.sheet_by_index(0) # Open first sheet. 
+
+# Display the number of sheets
+book.nsheets 
+
+# Display the list of sheet names
+book.sheet_names() 
+
+
+
+headings = sheet.row(0)
+data = {} 
+
+for i, row in enumerate(sheet.get_rows()):
+    if i == 0:
+        headings = row 
+        continue    
+
+        rate = {}
+    
+    for key, value in zip(headings, row):
+        rate[key.value] = value 
+        
+    
+    data[row[0].value] = rate 
+    
+   
+
+
+print(data)
